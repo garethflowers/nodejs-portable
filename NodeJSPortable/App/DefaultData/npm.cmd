8ffdc87757574a15572cb510ae44474258663ffd
Gareth Flowers <garethflowers@users.noreply.github.com>
2015-10-14 22:14:04 +0100

    Added latest NPM launch scripts

--- NodeJSPortable/App/DefaultData/npm.cmd
+++ NodeJSPortable/App/DefaultData/npm.cmd
@@ -1,6 +1,7 @@
-:: Created by npm, please don't edit manually.
 @IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe" "%~dp0\.\node_modules\npm\bin\npm-cli.js" %*
+  "%~dp0\node.exe"  "%~dp0\node_modules\npm\bin\npm-cli.js" %*
 ) ELSE (
-  node "%~dp0\.\node_modules\npm\bin\npm-cli.js" %*
+  @SETLOCAL
+  @SET PATHEXT=%PATHEXT:;.JS;=;%
+  node  "%~dp0\node_modules\npm\bin\npm-cli.js" %*
 )
