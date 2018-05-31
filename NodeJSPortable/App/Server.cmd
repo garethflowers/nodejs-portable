1829b0cfc841c45bc036f30780900e30c330b110
Gareth Flowers <garethflowers@users.noreply.github.com>
2018-05-31 01:33:13 +0100

    Adds online installer

--- NodeJSPortable/App/Server.cmd
+++ NodeJSPortable/App/Server.cmd
@@ -1,11 +1,12 @@
 @echo off
 
 title NodeJS
+for /f "delims=" %%? in ( 'node --version' ) do set NODEVER=%%?
+for /f "delims=" %%? in ( 'npm --version' ) do set NPMVER=%%?
 
-cls
 echo.
-echo Node
-node --version
+echo Node %NODEVER%
+echo NPM %NPMVER%
 echo.
 
 cmd /k
